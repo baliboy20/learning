d5d7cc1c6ecbcdea1d7cb12a1a94cfd6070dc58d
baliboy20 <williampaulton@yahoo.co.uk>
2019-03-05 18:08:02 +0000

    added animations

--- docs/animations/exAni/src/app/app.module.ts
+++ docs/animations/exAni/src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeModule} from "./home/home.module";
+import {AboutModule} from "./about/about.module";
+import {AboutComponent} from "./about/about.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
+const route: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HomeModule, AboutModule, RouterModule.forRoot(route), BrowserAnimationsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
